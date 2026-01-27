7e2562b73d0434e58002c8d11b034e3fff2fd694
MichaelKazembe <michaelkazembe@gmail.com>
2026-01-27 22:41:22 +0200

    update the current URL with the search params

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,12 +1,22 @@
 "use client";
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-
-function handleSearch(term: string) {
-  console.log(term);
-}
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  function handleSearch(term: string) {
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
