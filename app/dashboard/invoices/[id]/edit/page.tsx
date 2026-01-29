e2a30e4eb19c5c0444effab3f5efc18200bd6762
MichaelKazembe <michaelkazembe@gmail.com>
2026-01-29 22:28:55 +0200

    mplemented not-found

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import { notFound } from "next/navigation";
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
@@ -16,7 +17,7 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
   ]);
 
   if (!invoice) {
-    throw new Error(`Invoice with ID ${id} not found`);
+    notFound();
   }
 
   return (
